ee1224da2f327853fae46d9fd0b0f80b15266a96
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-01-15 01:57:27 -0500

    Use JS file to remove navbar repitition

---
+++ add-navbar.js
@@ -0,0 +1,12 @@
+document.body.insertAdjacentHTML('afterbegin', /* HTML */ `
+  <nav style="background-color: rgb(119, 119, 240);">
+    <li>
+      <div style="display: flex;">
+        <ul><a href="/">Home</a></ul>
+        <ul><a href="/about.html">About</a></ul>
+        <ul><a href="/explore.html">Explore Simulations</a></ul>
+        <ul><a href="/feedback.html">Feedback</a></ul>
+      </div>
+    </li>
+  </nav>
+`)
