9e84b8e2c63b9ec6dde090d016d4b959af1e5040
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-01-21 17:56:57 -0500

    Use a web component for navbar

---
+++ navbar.js
@@ -0,0 +1,28 @@
+class PhysicsNavbar extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = /* HTML */ `
+      <nav>
+        <li>
+          <ul><a href="/">Home</a></ul>
+          <ul><a href="/about.html">About</a></ul>
+          <ul><a href="/explore.html">Explore Simulations</a></ul>
+          <ul><a href="/feedback.html">Feedback</a></ul>
+        </li>
+      </nav>
+
+      <style>
+        nav {
+          background-color: rgb(119, 119, 240);
+        }
+
+        li {
+          display: flex;
+        }
+      </style>
+    `;
+  }
+}
+
+customElements.define('physics-navbar', PhysicsNavbar);
