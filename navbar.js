b320c8c72418a53f312426989a89f39d9e21b971
regexbuster <benjamin@gofranks.com>
2021-02-18 10:45:10 -0800

    testing navbar problems

--- navbar.js
+++ navbar.js
@@ -2,38 +2,39 @@ class PhysicsNavbar extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = /* HTML */ `
-      <nav>
-        <li>
-          <ul><a href="/">Home</a></ul>
-          <ul><a href="/about.html">About</a></ul>
-          <ul><a href="/explore.html">Explore Simulations</a></ul>
-          <ul><a href="/feedback.html">Feedback</a></ul>
-        </li>
-      </nav>
-
-      <!-- styles are scoped to the navbar -->
-      <style>
-        nav {
-          background-color: rgb(119, 119, 240);
-        }
-
-        li {
-          display: flex;
-          justify-content: center;
-        }
-
-        ul a {
-          color: white;
-          font-size: 1.5rem;
-          text-decoration: none;
-        }
-
-        ul a:hover {
-          color: #ccc;
-        }
-      </style>
-    `;
+    this.shadowRoot.innerHTML = `/* HTML */ 
+          <!-- NOTICE ME -->
+          <nav>
+            <li>
+              <ul><a href="/">Home</a></ul>
+              <ul><a href="/about.html">About</a></ul>
+              <ul><a href="/explore.html">Explore Simulations</a></ul>
+              <ul><a href="/feedback.html">Feedback</a></ul>
+            </li>
+          </nav>
+    
+          <!-- styles are scoped to the navbar -->
+          <style>
+            nav {
+              background-color: rgb(119, 119, 240);
+            }
+    
+            li {
+              display: flex;
+              justify-content: center;
+            }
+    
+            ul a {
+              color: white;
+              font-size: 1.5rem;
+              text-decoration: none;
+            }
+    
+            ul a:hover {
+              color: #ccc;
+            }
+          </style>
+        ;`
   }
 }
 
