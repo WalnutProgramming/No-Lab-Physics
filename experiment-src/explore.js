0e6f835843b92139fff635ed5f11bbf94a6945c0
regexbuster <benjamin@gofranks.com>
2021-02-19 17:13:53 -0800

    links work
    shoutout to greeshot on windows!

--- experiment-src/explore.js
+++ experiment-src/explore.js
@@ -1,22 +1,15 @@
-const images = ["./explore-img/balls.png","./explore-img/multiple.png","./explore-img/orbit.png","./explore-img/ramp.png","./explore-img/random.png","./explore-img/smaller.png"]
-const links = ["#sGsiVpiBNNUruWH4JifR2", "#mLI52rAtYpvBxSDhaxGoe", "l#AM4noTHlCQ3ja3UQ_X2GQ", "#8Mxh5fXQboFNyVEI8Sf2r", "#yQXvv_togO9L9smlO0-U2", "#53ATFufNSLGWwew4OUd-F"]
+const images = ["orbit","ramp","balls","tworect","random","smaller"]
+const links = ["mQdfnRQREewP83u65edLM","j-D-D-MvwwQTxAw3TGQQG","-9zOyPV1pHbZ71xcN848d","QdGcISXXwSc3Mkinv39BB","kRAITlQnFRaAActkidJzg","eDeWVP4aDnf-KRnXBB34f"]
 const sims = document.getElementsByClassName("top-sims");
-
-// var para = document.createElement("p");
-// var node = document.createTextNode("This is new.");
-// para.appendChild(node);
-
-// var element = document.getElementById("div1");
-// element.appendChild(para);
 
 function testing(){
 	for(let i = 0; i < images.length; i++){
 		let tag = document.createElement("a")
 		let img = document.createElement("img")
 
-		tag.href = ("https://no-lab-physics.netlify.app/experiment-create.html" + links[i])
+		tag.href = ("https://no-lab-physics.netlify.app/experiment-create.html#" + links[i])
 
-		img.src = images[i]
+		img.src = ("./explore-img/" + images[i] + ".png")
 		img.height = (sims[i].clientHeight);
 		img.width = (sims[i].clientWidth);
 	
